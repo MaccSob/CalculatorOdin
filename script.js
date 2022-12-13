f6008e85be75bd20a82fa8ac95548be4b52749ef
MaccSob <mcdarkdream1@gmail.com>
2022-12-13 23:15:07 +0100

    sum stolen but done

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 
-const display = document.querySelector('.output');
+const previousScreen= document.querySelector('[data-previous-operand]')
+const currentScreen = document.querySelector('[data-current-operand]')
 const numberButtons = document.querySelectorAll('.number')
 const operatorButtons = document.querySelectorAll('.operator');
 const dot = document.getElementById("dot");
 const equalsButton = document.querySelector(".equals");
 const clear = document.querySelector('.clear');
 
-let displayValue = "";
-let storedNumber = "";
-let clickedOperator = "";
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
 
+
+
+equalsButton.addEventListener('click', evaluate)
 
 function addNumbers(num1, num2) {
     return num1 + num2;
@@ -32,6 +37,8 @@ function divideNumbers(num1, num2) {
 console.log(divideNumbers(451,5));
 
 function operate(operator, num1, num2) {
+  num1 = Number(num1);
+  num2 = Number(num2);
     switch (operator) {
       case '+':
         return addNumbers(num1, num2)
@@ -48,47 +55,63 @@ function operate(operator, num1, num2) {
 }
 
 
-numberButtons.forEach(number => {
-    number.addEventListener('click', (e) => {  
-        displayValue += number.value;
-        display.textContent = displayValue;
-        displayValue = storedNumber;
-        console.log(displayValue);
-        
-    })
-});
-operatorButtons.forEach(operator => {
-    operator.addEventListener('click', (e) => {
-        clickedOperator = operator;
-        display.textContent = clickedOperator;
-        console.log(clickedOperator);
-    })
-});
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+)
+function appendNumber(number) {
+  if (currentScreen.textContent === '0' || shouldResetScreen)
+    resetScreen()
+  currentScreen.textContent += number
+}
+
+function resetScreen() {
+  currentScreen.textContent = ''
+  shouldResetScreen = false
+}
 
 
 dot.addEventListener("click", function() {
     if (display.textContent.includes('.')) return
   display.textContent += '.'
 });
-equalsButton.addEventListener('click', displayResults)
 
 
-function displayResults() {
-
-        display.textContent = operate(clickedOperator,parseFloat(displayValue),parseFloat(storedNumber));
-        
-
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate()
+  firstOperand = currentScreen.textContent
+  currentOperation = operator
+  previousScreen.textContent = `${firstOperand} ${currentOperation}`
+  shouldResetScreen = true
 }
+
+
 function clearDisplay() {
-    display.textContent = "";
-    storedNumber = "";
-    displayValue = "";
-    result = "";
-    clickedOperator = "";
+  currentScreen.textContent = '0'
+  previousScreen.textContent = ''
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && currentScreen.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  secondOperand = currentScreen.textContent
+  currentScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand))
 
+  previousScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+  currentOperation = null
 }
 
-
-
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000
+}
 clear.addEventListener('click', clearDisplay);
 console.log(operate("*",5,5));
